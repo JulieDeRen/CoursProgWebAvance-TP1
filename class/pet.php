b7429e399d873fec229dc9b2adc44b2e0919779e
JulieDeRen <julie.deschenes-renaud@hec.ca>
2022-10-17 16:18:37 -0400

    Ajout de commentaires

--- class/pet.php
+++ class/pet.php
@@ -7,10 +7,12 @@ class Pet extends Animal {
     public $birthday;
     public $age;
 
+    // Construction de l'objet à partir de ses propriétés passées en paramètres
     public function __construct($name, $birthday, $age){
         $this -> name = $name;   
         $this -> birthday = $birthday;
-        // Calcul âge Ref : https://codesource.io/how-to-calculate-age-in-php/#:~:text=In%20order%20to%20calculate%20age,method%20and%20date_create()%20method.&text=Note%3A%20The%20date_diff()%20method,object%20from%20a%20supplied%20string.
+        // Calcul âge création d'un objet age Calcul et ensuite extraction de l'année, mois, jours
+        // Ref : https://codesource.io/how-to-calculate-age-in-php/#:~:text=In%20order%20to%20calculate%20age,method%20and%20date_create()%20method.&text=Note%3A%20The%20date_diff()%20method,object%20from%20a%20supplied%20string.
         $currentDate = date("d-m-Y");
         $ageCalcul = date_diff(date_create($birthday), date_create($currentDate));
         $age= $ageCalcul->format("%y ans, %m mois et %d jours");
